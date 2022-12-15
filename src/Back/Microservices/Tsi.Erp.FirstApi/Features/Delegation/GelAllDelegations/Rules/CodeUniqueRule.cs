cf16fec28de863bd8a764855f6736d87e4f48ef7
Selmi Abdelaziz <selmi.abdelaziz@gmail.com>
2022-12-15 03:13:33 +0100

    Refactoring Rule classes in FirstApi project

--- src/Back/Microservices/Tsi.Erp.FirstApi/Features/Delegation/GelAllDelegations/Rules/CodeUniqueRule.cs
+++ src/Back/Microservices/Tsi.Erp.FirstApi/Features/Delegation/GelAllDelegations/Rules/CodeUniqueRule.cs
@@ -33,17 +33,4 @@ namespace Tsi.Erp.FirstApi.Features.ModePaiement.Create.Rules
             return true;
         }
     }
-
-    [RuleOn(ExecuteRuleWhen.BeforeInsert | ExecuteRuleWhen.BeforeUpdate)]
-    public class CodeUnique2 : IRule<Gouvernorat>
-    {
-        public int Order => 2;
-
-        public string ErrorMessage => "No error";
-
-        public Task<bool> ValidateAsync(Gouvernorat entity, CancellationToken cancellationToken = default)
-        {
-            return Task.FromResult(true);
-        }
-    }
 }
